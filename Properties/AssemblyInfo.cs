6414b5a01e913bf2e24ba4015599ac172fe7720c
Fruitsy <fruitsyberries@gmail.com>
2024-02-29 11:52:49 -0500

    Version number changes

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,16 +7,16 @@ using System.Runtime.Versioning;
 using GlueSlimes;
 using MelonLoader;
 
-[assembly: AssemblyVersion("1.2.2.0")]
-[assembly: MelonInfo(typeof(GlueEntry), "Glue Slimes", "1.2.2", "FruitsyOG", "https://www.nexusmods.com/slimerancher2/mods/16")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: MelonInfo(typeof(GlueEntry), "Glue Slimes", "1.2.3", "FruitsyOG", "https://www.nexusmods.com/slimerancher2/mods/16")]
 [assembly: MelonGame("MonomiPark", "SlimeRancher2")]
 [assembly: AssemblyTitle("GlueSlimes")]
 [assembly: AssemblyDescription("This is a Slime Rancher 2 mod that adds in a new slime called the \"Glue Slime\".")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("FruitsyOG")]
 [assembly: AssemblyProduct("GlueSlimes")]
-[assembly: AssemblyCopyright("Copyright © FruitsyOG 2022")]
+[assembly: AssemblyCopyright("Copyright © FruitsyOG 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3228c6f0-a08b-4d7d-adb3-bd61a2fea358")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
